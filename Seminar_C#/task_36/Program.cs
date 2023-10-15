d35339907608976d20dd2464056b12a006671ab4
Andrey <cliff_06@bk.ru>
2023-10-15 13:08:11 +0500

    создали файл task_36

--- Seminar_C#/task_36/Program.cs
+++ Seminar_C#/task_36/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+
+// Пример с факториалом:
+// nt Factorial(int n)
+// {
+// 	if(n == 1) return 1;
+// 	return n * Factorial(n - 1);
+// }
+
+// Console.WriteLine(Factorial(5)); // 120
+
+
+int Factorial(int n)//453,45,4,0
+{
+	if(n == 0) return 0;
+	return n % 10 + Factorial(n / 10);//453,45,4
+    //4 % 10 + 45 % 10 + 453 % 10 + 0 = 12
+}
+
+Console.WriteLine(Factorial(453));
