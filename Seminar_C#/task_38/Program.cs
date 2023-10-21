200597efb4506d7f9bbd91aaa3eaa5463e6ee91b
Andrey <cliff_06@bk.ru>
2023-10-21 13:19:17 +0500

    создали файл task_38

--- Seminar_C#/task_38/Program.cs
+++ Seminar_C#/task_38/Program.cs
@@ -26,3 +26,57 @@ for (int i = 0; i < n; i++)
     }
     Console.WriteLine(i + "[" + string.Join(", ", array) + "]");
 }
+
+// Сортировка перемешиванием (шейкерная сортировка)
+
+// void ShakerSort(vector<int>& values) {
+//   if (values.empty()) {
+//     return;
+//   }
+//   int left = 0;
+//   int right = values.size() - 1;
+//   while (left <= right) {
+//     for (int i = right; i > left; --i) {
+//       if (values[i - 1] > values[i]) {
+//         swap(values[i - 1], values[i]);
+//       }
+//     }
+//     ++left;
+//     for (int i = left; i < right; ++i) {
+//       if (values[i] > values[i + 1]) {
+//         swap(values[i], values[i + 1]);
+//       }
+//     }
+//     --right;
+//   }
+// }
+
+
+
+// Сортировка расчёскa
+// Сортировка расчёской — улучшение сортировки пузырьком. Её идея состоит в том, чтобы «устранить» элементы с небольшими значения в конце массива,
+//  которые замедляют работу алгоритма. Если при пузырьковой и шейкерной сортировках при переборе массива сравниваются соседние элементы,
+//  то при «расчёсывании» сначала берётся достаточно большое расстояние между сравниваемыми значениями, а потом оно сужается вплоть до минимального.
+
+// void CombSort(vector<int>& values) {
+//   const double factor = 1.247; // Фактор уменьшения
+//   double step = values.size() - 1;
+
+//   while (step >= 1) {
+//     for (int i = 0; i + step < values.size(); ++i) {
+//       if (values[i] > values[i + step]) {
+//         swap(values[i], values[i + step]);
+//       }
+//     }
+//     step /= factor;
+//   }
+//   // сортировка пузырьком
+//   for (size_t idx_i = 0; idx_i + 1 < values.size(); ++idx_i) {
+//     for (size_t idx_j = 0; idx_j + 1 < values.size() - idx_i; ++idx_j) {
+//       if (values[idx_j + 1] < values[idx_j]) {
+//         swap(values[idx_j], values[idx_j + 1]);
+//       }
+//     }
+//   }
+// }
+
